cde5b75f8d4965dcca736e160caf29da43d4d302
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-27 23:54:04 +0100

    Measure Island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -19,8 +19,8 @@ def island_perimeter(grid):
         for c in range(col):
             if grid[r][c] == 1:
                 perimeter += 4
-            if r > 0 and grid[r -1][c] == 1:
+            if r > 0 and grid[r - 1][c] == 1:
                 perimeter -= 2
-            if c > 0 and grid[r][c -1] == 1:
-                perimeter -=2
+            if c > 0 and grid[r][c - 1] == 1:
+                perimeter -= 2
     return perimeter
